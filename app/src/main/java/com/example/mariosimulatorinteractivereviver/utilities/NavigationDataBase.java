9f0d0431277526b6fed261bea181108ac6a2f47e
aviad dudkevich <aviad.dudkevich@mail.huji.ac.il>
2020-09-30 22:40:33 +0300

    finish constructors

--- app/src/main/java/com/example/mariosimulatorinteractivereviver/utilities/NavigationDataBase.java
+++ app/src/main/java/com/example/mariosimulatorinteractivereviver/utilities/NavigationDataBase.java
@@ -1,13 +1,20 @@
 package com.example.mariosimulatorinteractivereviver.utilities;
 
+import android.util.Log;
+
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class NavigationDataBase {
+    static final String TAG = NavigationDataBase.class.getSimpleName();
+    static final String JSON_ERROR_MSG = "Error while reading json file";
     static final private String[] BUTTONS_LIST = {"z", "a", "left", "none", "r", "b", "up", "right", "down"};
 
     public static class Scene{
@@ -18,6 +25,27 @@ public class NavigationDataBase {
             private String[] optionName;
             private int[] optionSceneId;
             private String[] optionButton;
+
+            public ControlTime(JSONObject controlTimeData){
+                try{
+                    timeStampStart = timeStampToMillisecond(controlTimeData.getString("timeStampStart"));
+                    timeStampEnd = timeStampToMillisecond(controlTimeData.getString("timeStampEnd"));
+                    JSONArray options = controlTimeData.getJSONArray("options");
+                    numberOfOptions = options.length();
+                    optionName = new String[numberOfOptions];
+                    optionSceneId = new int[numberOfOptions];
+                    optionButton = new String[numberOfOptions];
+                    for (int i = 0; i < numberOfOptions; i++) {
+                        JSONObject optionData = options.getJSONObject(i);
+                        optionName[i] = optionData.getString("name");
+                        optionSceneId[i] = optionData.getInt("scene");
+                        optionButton[i] = optionData.getString("button");
+                    }
+                }
+                catch (JSONException ex){
+                    Log.e(TAG, JSON_ERROR_MSG);
+                }
+            }
         }
         private String videoId;
         private int id;
@@ -35,11 +63,14 @@ public class NavigationDataBase {
                 playlistIndex = sceneData.getInt("playlistIndex");
                 toadsDestroyed = sceneData.getInt("toadsDestroyed");
                 gameOver = sceneData.getBoolean("gameOver");
-
-
+                JSONArray controlTimesData = sceneData.getJSONArray("controlTime");
+                controlTimes = new ArrayList<>();
+                for (int i = 0; i < controlTimesData.length(); i++) {
+                    controlTimes.add(new ControlTime(controlTimesData.getJSONObject(i)));
+                }
             }
             catch (JSONException ex){
-
+                Log.e(TAG, JSON_ERROR_MSG);
             }
 
         }
@@ -87,7 +118,7 @@ public class NavigationDataBase {
             }
         }
         catch (JSONException ex){
-
+            Log.e(TAG, JSON_ERROR_MSG);
         }
 
     }
@@ -98,6 +129,20 @@ public class NavigationDataBase {
 
     public Scene getScene(int id){
         return scenes.get(id);
+    }
+
+    public static int timeStampToMillisecond(String input){
+        final Pattern timeStampPattern = Pattern.compile("(\\d?\\d):(\\d\\d)");
+        Matcher timeStampMatcher = timeStampPattern.matcher(input);
+        if (timeStampMatcher.matches()){
+            return (Integer.parseInt(timeStampMatcher.group(1)) * 60 + Integer.parseInt(timeStampMatcher.group(2))) * 1000;
+        }
+        else{
+            if (input.equals("inf")){
+                return -1;
+            }
+            else return 0;
+        }
     }
 }
 
